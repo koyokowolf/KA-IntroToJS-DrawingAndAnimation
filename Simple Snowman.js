975e13d1db6cf7ea5568d7e6fa967fc1ab362e43
Koyoko Wolf <54044163+koyokowolf@users.noreply.github.com>
2019-12-07 17:51:27 -0800

    Create Simple Snowman.js

---
+++ Simple Snowman.js
@@ -0,0 +1,16 @@
+/* In this challenge, you'll draw a snowman using the ellipse() command. HINT: ellipse (x, y, w, h)*/
+
+/*Step 1: make a circle for the botom of the snowman, with an ellipse()command.*/
+
+//bottom circle:
+ellipse(200, 300, 150, 150);
+
+/*Step 2: Now draw a second cirlce, for the middle of your snowman, using another ellipse() command. Make sure that your second circle is a little smaller than the bottom circle, and that it sits on top of the bottom circle.*/
+
+// middle circle:
+ellipse(200, 200, 125, 125);
+
+/* Step 3: Now draw a third circle, for the head of your snowman, using another ellipse() command. Make sure that your third circle is a little smaller than the middle circle, and gthat it sits on top of the middle circle, because that's what heads do.*/
+
+// top circle:
+ellipse(200, 100, 100, 100);
